62ec47708344e5072866d2f60af9ee6d2a23be58
Attila Marton <martonattila2010@gmail.com>
2018-06-19 10:32:14 +0200

    Commit with changes on pw.R and run_server_local.R
    CC. Marinka and Alex

---
+++ App1/pw.R
@@ -0,0 +1,10 @@
+# password file
+
+pw <- list(
+  port='5432',
+  script_path=getwd(),
+  project='ring',
+  puser='ring_admin',
+  ppass='s8uwdm4scqg',
+  database='gisdata',
+  server='localhost')
